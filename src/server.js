86ac8ce4ce75e682988e421c000d1f04ebcb9f2f
Squard974 <werr.sebastien@gmail.com>
2024-02-29 10:16:16 +0100

    etape 2

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import Fastify from "fastify"
 import fastifyBasicAuth from "@fastify/basic-auth"
+import {readFileSync} from "node:fs"
 
 
 const port = 3000;
 const authenticate = {realm: 'Westeros'}
 
+
+
 const fastify = Fastify({
-    logger: true
+    logger: true,
+    https: {
+        key: readFileSync('./src/server.key'),
+        cert: readFileSync('./src/server.crt')
+    }
 })
 
 fastify.register(fastifyBasicAuth, {
@@ -24,11 +31,25 @@ fastify.get('/dmz', {}, (req, res) => {
     res.send({replique: "Ca pourrait être mieux protégé..."})
 })
 
+fastify.get('/', function (request, reply) {
+    reply.code(200).send({ hello: 'world' })
+})
+
 fastify.after(() => {
     fastify.route({
         method: 'GET',
         url: '/secu',
         onRequest: fastify.basicAuth,
+        handler: async (req, reply) => {
+            return {
+                replique: 'Un Lannister paye toujours ses dettes !'
+            }
+        }
+    })
+    fastify.route({
+        method: 'GET',
+        url: '/autre',
+        //onRequest: fastify.basicAuth, // on enlève la sécurité
         handler: async (req, reply) => {
             return {
                 replique: 'Un Lannister paye toujours ses dettes !'
